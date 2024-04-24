4b379e376cdaf460fabc6d309e3d16a5fca33373
0xmnt <idntmnt@gmail.com>
2024-04-24 21:50:52 +0200

    nvim updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -19,9 +19,9 @@ config.window_padding = {
 	bottom = 0,
 }
 
-wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
-	window:gui_window():maximize()
-end)
+-- wezterm.on("gui-startup", function()
+-- 	local tab, pane, window = mux.spawn_window({})
+-- 	window:gui_window():maximize()
+-- end)
 
 return config
