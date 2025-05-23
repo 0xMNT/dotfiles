e171109a3efa4db4f67c139bfc65ab5b469607bc
0xMNT <idntmnt@gmail.com>
2025-05-23 14:24:00 +0200

    install: add gdu

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # packages
-yes | sudo pacman -Sy gdu xdg-desktop-portal-wlr grim slurp wl-clipboard libnotify waybar swayidle kubectx ripgrep-all mpv jq yq tldr nushell argocd helm kubectl man github-cli aws-cli docker mangohud nvidia nvidia-utils lib32-nvidia-utils lazygit fastfetch easyeffects zsh stow wezterm neovim tmux exa openssh firefox curl ripgrep make unzip gcc wget starship libappindicator-gtk3 libdbusmenu-gtk3 libnl jsoncpp libsigc++ fmt chrono-date spdlog libnotify pamixer
+yes | sudo pacman -Sy xdg-desktop-portal-wlr grim slurp wl-clipboard libnotify waybar swayidle kubectx ripgrep-all mpv jq yq tldr nushell argocd helm kubectl man github-cli aws-cli docker mangohud nvidia nvidia-utils lib32-nvidia-utils lazygit fastfetch easyeffects zsh stow wezterm neovim tmux exa openssh firefox curl ripgrep make unzip gcc wget starship libappindicator-gtk3 libdbusmenu-gtk3 libnl jsoncpp libsigc++ fmt chrono-date spdlog libnotify pamixer gdu
 
 # jetbrains mono nerd font
-mkdir -p $HOME/.fonts
+mkdir-p $HOME/.fonts
 wget -O $HOME/.fonts/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
 unzip $HOME/.fonts/JetBrainsMono.zip -d $HOME/.fonts
 fc-cache
