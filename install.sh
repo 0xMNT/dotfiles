7440bc61e8aaf31a37a7b77a71230bc48a68dbbc
0xMNT <idntmnt@gmail.com>
2025-10-07 22:02:15 +0200

    install: add config for hyprland

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Get hostname for host-specific configs
+HOST_NAME=$(cat /etc/hostname)
+
+# Create symlink for Hyprland host-specific config
+ln -sf ~/.config/hypr/hosts/${HOST_NAME}.conf ~/.config/hypr/host.conf
+
+# Merge waybar configs
+~/.config/waybar/merge-config.sh
+
 # packages
-yes | sudo pacman -Sy xdg-desktop-portal-wlr grim slurp wl-clipboard libnotify waybar swayidle kubectx ripgrep-all mpv jq yq tldr nushell argocd helm kubectl man github-cli aws-cli docker mangohud nvidia nvidia-utils lib32-nvidia-utils lazygit fastfetch easyeffects zsh stow wezterm neovim tmux exa openssh firefox curl ripgrep make unzip gcc wget starship libappindicator-gtk3 libdbusmenu-gtk3 libnl jsoncpp libsigc++ fmt chrono-date spdlog libnotify pamixer gdu
+sudo pacman -Syu xdg-desktop-portal-wlr grim slurp wl-clipboard libnotify waybar swayidle kubectx ripgrep-all mpv jq yq tldr nushell argocd helm kubectl man github-cli aws-cli docker mangohud nvidia nvidia-utils lazygit fastfetch easyeffects zsh stow wezterm neovim tmux exa openssh firefox curl ripgrep make unzip gcc wget starship libappindicator-gtk3 libdbusmenu-gtk3 libnl jsoncpp libsigc++ fmt chrono-date spdlog libnotify pamixer gdu
 
 # jetbrains mono nerd font
 mkdir-p $HOME/.fonts
