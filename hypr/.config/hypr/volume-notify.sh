e4dd836c0285ca39f44520c16e9af61bd1cc4b13
0xMNT <idntmnt@gmail.com>
2025-10-09 18:51:00 +0200

    chore: fix dunst issues

--- hypr/.config/hypr/volume-notify.sh
+++ hypr/.config/hypr/volume-notify.sh
@@ -5,7 +5,7 @@ VOLUME=$(pamixer --get-volume)
 MUTED=$(pamixer --get-mute)
 
 if [ "$MUTED" = "true" ]; then
-    notify-send -u low -h string:x-canonical-private-synchronous:volume -h int:value:0 "Volume" "Muted 󰖁"
+    dunstify -u low -r 2592 -h int:value:0 "Volume" "Muted 󰖁"
 else
-    notify-send -u low -h string:x-canonical-private-synchronous:volume -h int:value:$VOLUME "Volume" "${VOLUME}% 󰕾"
+    dunstify -u low -r 2592 -h int:value:$VOLUME "Volume" "${VOLUME}% 󰕾"
 fi
