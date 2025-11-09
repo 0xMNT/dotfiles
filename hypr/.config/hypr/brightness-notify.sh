13920986f1086e8d671c15a75d46785f16b57543
0xMNT <idntmnt@gmail.com>
2025-11-09 11:02:56 +0100

    swayosd: use sway instead of dunstify

--- hypr/.config/hypr/brightness-notify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get current brightness
-MAX_BRIGHTNESS=$(brightnessctl max)
-CURRENT_BRIGHTNESS=$(brightnessctl get)
-BRIGHTNESS_PERCENT=$((CURRENT_BRIGHTNESS * 100 / MAX_BRIGHTNESS))
-
-dunstify -u low -r 2593 -h int:value:$BRIGHTNESS_PERCENT -h string:hlcolor:#98FFB3 "Brightness" "${BRIGHTNESS_PERCENT}% 󰃠"
