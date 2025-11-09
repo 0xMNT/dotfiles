b043884ce7a60540066afdeb8e3692ca9726bf75
0xMNT <idntmnt@gmail.com>
2025-11-09 21:12:20 +0100

    swayosd: add orientation notification

--- hypr/.config/hypr/toggle-rotation.sh
+++ hypr/.config/hypr/toggle-rotation.sh
@@ -9,9 +9,11 @@ if [ ! -f "$STATE_FILE" ] || [ "$(cat $STATE_FILE)" = "0" ]; then
     sed -i 's/transform,0/transform,1/; s/transform = 0/transform = 1/' "$CONFIG_FILE"
     hyprctl reload
     echo "1" > "$STATE_FILE"
+    ~/.local/bin/swayosd-client --custom-message "Portrait" --custom-icon "object-rotate-right"
 else
     # Rotate back to normal
     sed -i 's/transform,1/transform,0/; s/transform = 1/transform = 0/' "$CONFIG_FILE"
     hyprctl reload
     echo "0" > "$STATE_FILE"
+    ~/.local/bin/swayosd-client --custom-message "Landscape" --custom-icon "object-rotate-left"
 fi
